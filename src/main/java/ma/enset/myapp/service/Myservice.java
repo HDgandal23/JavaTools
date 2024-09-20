be3c416b3279eccbc248c4e40af640839fff298b
Gandal Dev <gandaltech23@gmail.com>
2024-09-20 08:59:37 -0700

    first commit on prod branch

--- src/main/java/ma/enset/myapp/service/Myservice.java
+++ src/main/java/ma/enset/myapp/service/Myservice.java
@@ -3,7 +3,15 @@ package ma.enset.myapp.service;
 
 public class Myservice {
 
+    
+
     public double compute(double a , double b){
         return a+b;
+    }
+
+    // new method 
+
+    public void SayHello(){
+        System.out.println("Hello, world I am a java backend developer !!!");
     }
 }
